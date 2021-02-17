6c90ef63ee41fc90fe9828168be752649f5494b3
ARYA RAJPUT <rajputarya10@gmail.com>
2021-02-17 17:28:26 +0530

    third commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
-import { Avatar } from '@material-ui/core'
-import { ExpandMore, SearchRounded, SettingsOutlined ,HelpOutline} from '@material-ui/icons'
+import { Avatar,IconButton } from '@material-ui/core'
+import {ExpandMore, SearchRounded, SettingsOutlined ,HelpOutline} from '@material-ui/icons'
 import AppsIcon from '@material-ui/icons/Apps';
 import React from 'react'
+import './header.css'
 
 function Header() {
     return (
-        <div>
+        <div className='Header'>
             <div className='logo-sec'>
                 <img src='https://cdn1.iconfinder.com/data/icons/logotypes/32/google-drive-512.png'/>
-                <h3>DRIVE</h3>
+                <span>Drive</span>
             </div>
             <div className='search-sec'>
-                <SearchRounded/>
-                <input type='text'/>
-                <ExpandMore/>
+               <IconButton> <SearchRounded/></IconButton>
+                <input type='text' placeholder='Search in Drive'/>
+                <IconButton><ExpandMore/></IconButton>
             </div>
-            <div className='other'>
-                <HelpOutline/>
-                <SettingsOutlined/>
-                <AppsIcon/>
+            <div className='other-sec'>
+            <IconButton> <HelpOutline/></IconButton>
+            <IconButton><SettingsOutlined/></IconButton>
+            <IconButton> <AppsIcon/></IconButton>
                 <Avatar/>
             </div>
 
